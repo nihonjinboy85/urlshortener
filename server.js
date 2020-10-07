51d08424233ba16c3eaf8b18fcc3f79593d006c4
Richard Carrigan <nihonjinboy85@gmail.com>
2020-10-06 22:31:49 -0700

    Add .gitignore

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
