cc809c395da872b9b94d8f42ce8ba0d247826d1d
Richard Carrigan <nihonjinboy85@gmail.com>
2020-10-06 22:19:23 -0700

    Initial config

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const dns = require('dns');
+const express = require('express');
+const mongoose = require('mongoose');
+const url = require('url');
 
-var cors = require('cors');
-
-var app = express();
+const app = express();
 
 // Basic Configuration 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(console.log('Connected to db...'));
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -31,7 +35,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.post('/api/shorturl/new', (req, res) => {
+  const myURL = new URL(req.body.url);
+  dns.lookup(myURL.host, (err) => {
+    res.json(err ? {"error":"invalid URL"} : {"original_url": req.body.url,"short_url":1});
+  });
+});
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log(`Node.js listening at port ${process.env.PORT}`);
 });
