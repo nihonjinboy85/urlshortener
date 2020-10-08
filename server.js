085a7a327d576f8d30ea8bc1e5b430afcf7af557
Richard Carrigan <nihonjinboy85@gmail.com>
2020-10-07 23:26:15 -0700

    Configure posting to db

--- server.js
+++ server.js
@@ -8,16 +8,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const url = require('url');
 
-const app = express();
 
 // Basic Configuration 
 const port = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
 
 /** this project needs a db !! **/ 
 mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(console.log('Connected to db...'));
 
-app.use(cors());
+const urlSchema = new mongoose.Schema({ 
+  id: { type: Number, default: 1 },
+  longUrl: String 
+});
+const UrlModel = mongoose.model('UrlModel', urlSchema);
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
@@ -37,8 +42,25 @@ app.get("/api/hello", function (req, res) {
 app.post('/api/shorturl/new', (req, res) => {
   const myURL = new URL(req.body.url);
   dns.lookup(myURL.host, (err) => {
-    res.json(err ? {"error":"invalid URL"} : {"original_url": req.body.url,"short_url":1});
+    if(err) {
+      res.json({ 'error': 'invalid URL' });
+    } else {
+      const urlRecord = new UrlModel({ longUrl: myURL });
+      urlRecord.save((err, urlRecord) => {
+        if(err) {
+          res.json({ 'error': err });
+        } else {
+          res.json({ 'original_url': myURL.host, 'short_url': urlRecord.id });
+        }
+      })
+    }
   });
+});
+
+app.get('/api/shorturl/:id', (req, res) => {
+  if(req.params.id === 'id') {
+    res.redirect('https://forum.freecodecamp.org/');
+  }
 });
 
 app.listen(port, function () {
