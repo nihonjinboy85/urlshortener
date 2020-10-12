33c46c242575a5bf80e1ba5337945caf704f4c00
Richard Carrigan <nihonjinboy85@gmail.com>
2020-10-11 23:42:57 -0700

    Replace mongoose with mongodb

--- server.js
+++ server.js
@@ -5,64 +5,64 @@ const cors = require('cors');
 const dotenv = require('dotenv').config();
 const dns = require('dns');
 const express = require('express');
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 const url = require('url');
-
 
 // Basic Configuration 
 const port = process.env.PORT || 3000;
 const app = express();
+
+// CORS Middleware
 app.use(cors());
 
-/** this project needs a db !! **/ 
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(console.log('Connected to db...'));
-
-const urlSchema = new mongoose.Schema({ 
-  id: { type: Number, default: 1 },
-  longUrl: String 
+// Set the MongoDB Client that will be used
+const client = new MongoClient(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  if(err) {
+    res.json({ status: 'failure', msg: err });
+  } else {
+    const collection = client.db('url-shortener').collection('urls');
+  }
 });
-const UrlModel = mongoose.model('UrlModel', urlSchema);
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+// body-parser Middleware
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Set static HTML folder
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// Handle root route GET requests
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
+// Handle POST requests to add new URLs to collection and return short URL
 app.post('/api/shorturl/new', (req, res) => {
-  const myURL = new URL(req.body.url);
-  dns.lookup(myURL.host, (err) => {
-    if(err) {
-      res.json({ 'error': 'invalid URL' });
-    } else {
-      const urlRecord = new UrlModel({ longUrl: myURL });
-      urlRecord.save((err, urlRecord) => {
-        if(err) {
-          res.json({ 'error': err });
-        } else {
-          res.json({ 'original_url': myURL.host, 'short_url': urlRecord.id });
-        }
-      })
-    }
-  });
-});
-
-app.get('/api/shorturl/:id', (req, res) => {
-  if(req.params.id === 'id') {
-    res.redirect('https://forum.freecodecamp.org/');
+  try {
+    const myURL = new URL(req.body.url);
+    dns.lookup(myURL.host, (err) => {
+      if(err) {
+        res.json({ error: 'invalid URL' });
+      } else {
+        res.json({ original_url: myURL, short_url: 1 });
+      }
+    });
+  }
+  catch {
+    res.json({ error: 'invalid URL' });
   }
 });
 
+// Lookup URL ID provided and redirect to corresponding long URL
+app.get('/api/shorturl/:id', (req, res) => {
+  if(Number(req.params.id) === 3) {
+    res.redirect('https://forum.freecodecamp.org/');
+  } else {
+    res.json({ status: 'failure', msg: `${req.params.id} doesn't equal 3`});
+  }
+});
+
+// Start server
 app.listen(port, function () {
   console.log(`Node.js listening at port ${process.env.PORT}`);
 });
